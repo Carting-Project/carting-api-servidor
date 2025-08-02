415713c70fa3e18ae3d084770f582d3a8b2f60a0
Checo9928 <sergio.estudiante0@gmail.com>
2025-08-02 15:25:37 -0500

    Update __init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,1 +1,25 @@
+# Este archivo simulará nuestra conexión a la base de datos por ahora.
+class DBSession:
+    def execute(self, query, values):
+        print("--- Ejecutando Query ---")
+        print(query)
+        print("--- Con Valores ---")
+        print(values)
+        class MockResult:
+            def fetchone(self):
+                return (1,)
+        return MockResult()
+    def commit(self):
+        print("--- Haciendo Commit (Guardando cambios) ---")
+    def rollback(self):
+        print("--- Haciendo Rollback (Revirtiendo cambios) ---")
 
+def get_db():
+    db = DBSession()
+    try:
+        yield db
+    finally:
+        pass
+
+class Session:
+    pass
